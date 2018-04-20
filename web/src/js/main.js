b9a0055214ade454e7633103ffa1cf41537bd35f
joserealdev <joserealdev@gmail.com>
2018-04-20 14:28:09 +0200

    More changes

--- web/src/js/main.js
+++ web/src/js/main.js
@@ -1,38 +1,13 @@
 $(document).ready(function() {
   let html = "";
   for (let x = 1; x < 31; x++) {
-    if (x < 10) html += '<button class="btn-selq">' + x + "</button>";
-    else if (x < 20)
-      html +=
-        '<button style="width:55px!important" class="btn-selq">' +
-        x +
-        "</button>";
-    else
-      html +=
-        '<button style="width:54px!important" class="btn-selq">' +
-        x +
-        "</button>";
+    html += '<button class="btn-selq">' + x + "</button>";
   }
   document.getElementById("btns").innerHTML = html;
   $(".owl-carousel").owlCarousel({
-    center: true,
-    loop: true,
-    rewind: true,
     dots: false,
     nav: false,
-    margin: 0,
-    responsiveClass: true,
-    responsive: {
-      0: {
-        items: 1
-      },
-      600: {
-        items: 3
-      },
-      1000: {
-        items: 5,
-        loop: false
-      }
-    }
+    autoWidth: true,
+    margin: 5
   });
 });
